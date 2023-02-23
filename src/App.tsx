21432ea0557c40001b610b8c5ac0d1de07546a02
Marc Löhe <loehe@sipgate.de>
2023-02-23 16:05:26 +0100

    refactor: move tracks to own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
 import SpotifyPlayer from "./SpotifyPlayer";
 import {useState} from "react";
+import {tracks} from "./tracks";
 
 interface Props {
     spotifyIframeApi: IframeApi
 }
-
-interface Track {
-    uri: string
-    artist: string
-    title: string
-}
-
-const tracks: Track[] = [
-    {
-        uri: "spotify:track:3JPcICirkAw4TLp9UzEcfl",
-        artist: "Mr. Oizo",
-        title: "Flat Beat"
-    },
-    {
-        uri: "spotify:track:0usJ6ywAqqeJx1nQl5SWlP",
-        artist: "No Doubt",
-        title: "Don't speak"
-    },
-    {
-        uri: "spotify:track:3MjUtNVVq3C8Fn0MP3zhXa",
-        artist: "Britney Spears",
-        title: "...Baby One More Time"
-    }
-]
 
 function App(props: Props) {
     const [currentTrack, setCurrentTrack] = useState(0);
